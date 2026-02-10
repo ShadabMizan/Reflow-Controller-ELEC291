2f978a70abf4bfb14d6152b631da61c3ee2ff670
Theo Estoup Ruiz <estoupruiztheo@gmail.com>
2026-02-09 17:02:33 -0800

    yay

---
+++ DE10Lite_asm_Examples/DE10Lite/__LOAD.bat
@@ -0,0 +1,3 @@
+@echo off
+::This file was created automatically by CrossIDE to load a hex file using Quartus_stp.
+"C:\intelFPGA_lite\24.1std\quartus\bin64\quartus_stp.exe" -t "C:\CrossIDE\Load_Script.tcl" "C:\Users\estou\elec291\Reflow-Controller-ELEC291\DE10Lite_asm_Examples\DE10Lite\ADC_7seg.HEX" | find /v "Warning (113007)"
